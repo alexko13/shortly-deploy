2ef24a068a493920f3d89e780fd946aecd34c41e
Alex Ko <alexko@utexas.edu>
2016-02-10 16:41:53 -0700

    now use environment variables

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var app = require('./server.js');
+  
+// var port = 4568;
 
- var port = 4568;
+// app.listen(port);
 
-app.listen(port);
+// console.log('Server now listening on port ' + port);
 
-console.log('Server now listening on port ' + port);
+
+app.listen(process.env.PORT || 4568, function() {
+  console.log('Node app is running on port', process.env.PORT || 4568);
+});
